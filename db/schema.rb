b3061e327e781b586a0715f8759549fce6aae9b2
tpdo33 <tpdo33@gmail.com>
2016-05-10 13:21:45 -0700

    migrated description and timestamp columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160510201133) do
+ActiveRecord::Schema.define(version: 20160510201652) do
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "create_at"
+    t.datetime "updated_at"
   end
 
 end

a0e0b084ddd78c6c3f00d3549d5e4c2423f570f2
tpdo33 <tpdo33@gmail.com>
2016-05-14 23:04:12 -0700

    added edit user fuction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,20 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			flash[:sucess] = " Your account was updated successfully"
+			redirect_to "/articles"
+		else 
+			render 'edit'
+		end
+	end
+
 
 	private
 		def user_params

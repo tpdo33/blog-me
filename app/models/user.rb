ef363c6d1eadab635937a2244961afbf17c08a8f
tpdo33 <tpdo33@gmail.com>
2016-05-14 19:44:40 -0700

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+	validates :username, presence: true, 
+			  uniqueness: {case_sensitive: false} ,
+			  length: {minimum: 3, maximum: 25}
+
+	VALID_EMAIL_REGEX = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/i
+	validates :email, presence: true, 
+			  uniqueness: {case_sensitive: false},
+			  format: { with: VALID_EMAIL_REGEX},
+			  length: {maximum: 110},
+
+
 end

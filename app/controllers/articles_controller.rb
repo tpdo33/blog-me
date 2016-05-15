cff8f597f82bd195d5f8ae490e51c542f96027c0
tpdo33 <tpdo33@gmail.com>
2016-05-15 02:25:02 -0700

    added pagination to users and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
 
 	# Grab all articles from database
 	def index
-		@articles = Article.all
+		@articles =Article.paginate(:page => params[:page], :per_page => 5)
 	end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController 
+	before_action :set_user, only: [:edit, :update, :show, :destroy]
 
 	def index
-		@users = User.all
+		
+		@users =User.paginate(:page => params[:page], :per_page => 5)
 	end
 
 	def new
@@ -20,11 +22,11 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		@user = User.find(params[:id])
+		# @user = User.find(params[:id])
 	end
 
 	def update
-		@user = User.find(params[:id])
+		# @user = User.find(params[:id])
 		if @user.update(user_params)
 			flash[:sucess] = " Your account was updated successfully"
 			redirect_to "/articles"
@@ -34,12 +36,17 @@ class UsersController < ApplicationController
 	end
 
 	def show
-		@user = User.find(params[:id])
+		# @user = User.find(params[:id])
+	
 	end
 
 
 	private
 		def user_params
 			params.require(:user).permit(:username, :email, :password)
+		end
+		def set_user
+			@user = User.find(params[:id])
+			
 		end
 end

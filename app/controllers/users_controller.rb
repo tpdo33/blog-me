b9560314b8485b6689cafa8a9e4ba4c3e0e40790
tpdo33 <tpdo33@gmail.com>
2016-05-15 21:48:41 -0700

    Users cannot delete/edit other users' account info and articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController 
 	before_action :set_user, only: [:edit, :update, :show]
+	before_action :require_same_user, only: [:edit, :update]
 
 	def index
 		
@@ -47,5 +48,11 @@ class UsersController < ApplicationController
 		end
 		def set_user
 			@user = User.find(params[:id])	
+		end
+		def require_same_user
+			if current_user != @user
+				flash[:danger] = "You can only edit your own account"
+				redirect_to root_path
+			end
 		end
 end
